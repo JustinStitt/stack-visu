4726245541810bc30f5567f6f815788f970758aa
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-03-15 15:59:37 -0700

    🎨 main app functionality

--- stack-visu-app/src/App.svelte
+++ stack-visu-app/src/App.svelte
@@ -1,24 +1,56 @@
 <script>
-	export let name;
+	import { fade, fly} from 'svelte/transition';
+	import Block from './Components/Block.svelte'
+	let blocks = [];
+	let inp = 0;
+	const addBlock = () => {
+		blocks = [...blocks, inp]
+	}
+
+	const popBlock = () => {
+		blocks = blocks.slice(0, blocks.length-1);
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class='blocks'>
+		<h1>Stack Visualization!</h1>
+		<div class='inner'>
+			{#each blocks as block}
+			<Block data={block}/>
+			{/each}
+		</div>
+		<h4 style='color: aliceblue;'>
+			Last Update: 3/15/2022 15:59 PST
+		</h4>
+	</div>
+	<div class='controls'>
+		<button class='add-block' on:click={addBlock}>
+			Add Block
+		</button>
+		<input type="text" bind:value={inp} style='width: 15%; text-align: center'>
+		<button  on:click={popBlock}>
+			Remove Block
+		</button>
+	</div>
+	
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
 	}
 
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
 	}
 
@@ -27,4 +59,23 @@
 			max-width: none;
 		}
 	}
+
+	.blocks {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		align-items: flex-end;
+		background-color: #333;
+	}
+	
+	.inner {
+		width: 100%;
+		height: 325px;
+		overflow-y: auto;
+		border: 1px solid red;
+		display: flex;
+		flex-direction: column-reverse;
+	}
+
+
 </style>
---
+++ stack-visu-app/src/Components/Block.svelte
@@ -0,0 +1,40 @@
+<script>
+export let data = 7;
+export let size = 400;
+let color = Math.floor(Math.random()*16777215).toString(16);
+
+const checkDarkColor = (_) => {
+    if (color < 6000) {
+        color = Math.floor(Math.random()*16777215).toString(16);
+    }
+}
+
+$: checkDarkColor(color)
+</script>
+
+<!-- start html -->
+<div style={`background-color: #${color}`}>
+    <h1>{data}</h1>
+</div>
+<!-- end html -->
+
+<style>
+    div {
+        width: 300px;
+        height: 40px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border: 1.5px solid aliceblue;
+    }
+
+    div:hover {
+        filter:brightness(1.2);
+        transform: scale(1.07);
+    }
+
+    h1 {
+        text-align: center;
+    }
+</style>
