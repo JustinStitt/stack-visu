59eb3144ea47027a5cdf7863705bd20ffeda9306
Justin Stitt <cpt.crixus@gmail.com>
2022-10-03 22:16:18 -0700

    update and add queue

--- stack-visu-app/src/App.svelte
+++ stack-visu-app/src/App.svelte
@@ -1,81 +1,121 @@
 <script>
-	import { fade, fly} from 'svelte/transition';
-	import Block from './Components/Block.svelte'
-	let blocks = [];
-	let inp = 0;
-	const addBlock = () => {
-		blocks = [...blocks, inp]
-	}
+  import { fade, fly } from "svelte/transition";
+  import Block from "./Components/Block.svelte";
+  let blocks = [];
 
-	const popBlock = () => {
-		blocks = blocks.slice(0, blocks.length-1);
-	}
+  let inp = 0;
+
+  let isStack = true;
+
+  const stackPush = () => {
+    blocks = [...blocks, inp];
+  };
+
+  const queuePush = () => {
+    // blocks = [inp, ...blocks];
+    blocks = [...blocks, inp];
+  };
+
+  const stackPop = () => {
+    blocks = blocks.slice(0, blocks.length - 1);
+  };
+
+  const queuePop = () => {
+    blocks = blocks.slice(1, blocks.length);
+    console.log(blocks);
+  };
+
+  const addBlock = () => {
+    if (isStack) stackPush();
+    else queuePush();
+    inp += 1;
+  };
+
+  const popBlock = () => {
+    if (isStack) stackPop();
+    else queuePop();
+  };
 </script>
 
 <main>
-	<div class='blocks'>
-		<h1>Stack Visualization!</h1>
-		<div class='inner'>
-			{#each blocks as block}
-			<Block data={block}/>
-			{/each}
-		</div>
-		<h4 style='color: aliceblue;'>
-			Last Update: 3/15/2022 15:59 PST
-		</h4>
-	</div>
-	<div class='controls'>
-		<button class='add-block' on:click={addBlock}>
-			Add Block
-		</button>
-		<input type="text" bind:value={inp} style='width: 15%; text-align: center'>
-		<button  on:click={popBlock}>
-			Remove Block
-		</button>
-	</div>
-	
+  <div class="blocks">
+    {#if isStack}
+      <h1>Stack Visualization!</h1>
+    {/if}
+    {#if !isStack}
+      <h1>Queue Visualization!</h1>
+    {/if}
+    <div class={isStack ? "inner-stack" : "inner-queue"}>
+      {#each blocks as block, i (block)}
+        <Block data={block} {isStack} />
+      {/each}
+    </div>
+  </div>
+  <div class="controls">
+    <button class="add-block" on:click={addBlock}> Add Block </button>
+    <input
+      type="text"
+      bind:value={inp}
+      style="width: 15%; text-align: center"
+    />
+    <button on:click={popBlock}> Remove Block </button>
+    <button
+      on:click={() => {
+        isStack = !isStack;
+      }}>Swap to Queue</button
+    >
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		max-width: 240px;
-		padding: 0;
-		margin: 0;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-	}
+  main {
+    text-align: center;
+    max-width: 240px;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    overflow: hidden;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 3em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 3em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 
-	.blocks {
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-end;
-		align-items: flex-end;
-		background-color: #333;
-	}
-	
-	.inner {
-		width: 100%;
-		height: 325px;
-		overflow-y: auto;
-		border: 1px solid red;
-		display: flex;
-		flex-direction: column-reverse;
-	}
+  .blocks {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+    background-color: #333;
+    max-width: 500px;
+  }
 
+  .inner-stack {
+    width: 100%;
+    height: 325px;
+    overflow-y: auto;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column-reverse;
+  }
 
+  .inner-queue {
+    width: 100%;
+    height: 325px;
+    overflow-y: auto;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+  }
 </style>
--- stack-visu-app/src/Components/Block.svelte
+++ stack-visu-app/src/Components/Block.svelte
@@ -1,40 +1,62 @@
 <script>
-export let data = 7;
-export let size = 400;
-let color = Math.floor(Math.random()*16777215).toString(16);
+  import { slide, fly, fade } from "svelte/transition";
+  export let data = 7;
+  export let isStack = true;
+  export let size = 400;
+  let color = Math.floor(Math.random() * 16777215).toString(16);
 
-const checkDarkColor = (_) => {
+  const checkDarkColor = (_) => {
     if (color < 6000) {
-        color = Math.floor(Math.random()*16777215).toString(16);
+      color = Math.floor(Math.random() * 16777215).toString(16);
     }
-}
+  };
 
-$: checkDarkColor(color)
+  $: checkDarkColor(color);
 </script>
 
 <!-- start html -->
-<div style={`background-color: #${color}`}>
-    <h1>{data}</h1>
+<div
+  class={isStack ? "stack" : "queue"}
+  style={`background-color: #${color}`}
+  id="block"
+  transition:slide
+  on:click={() => {}}
+>
+  <h1>{data}</h1>
 </div>
+
 <!-- end html -->
-
 <style>
-    div {
-        width: 300px;
-        height: 40px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        border: 1.5px solid aliceblue;
-    }
+  #block {
+    transition: all 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
 
-    div:hover {
-        filter:brightness(1.2);
-        transform: scale(1.07);
-    }
+  .stack {
+    width: 300px;
+    height: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1.5px solid aliceblue;
+  }
 
-    h1 {
-        text-align: center;
-    }
+  .queue {
+    width: 40px;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1.5px solid aliceblue;
+  }
+
+  div:hover {
+    filter: brightness(1.2);
+    transform: scale(1.07);
+  }
+
+  h1 {
+    text-align: center;
+  }
 </style>
